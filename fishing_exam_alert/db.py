797b634399d804a7080b307e5f19de8410576a2c
larsheinen <https://meet.google.com/yqv-jgpo-cjr?hs=151>
2022-02-25 00:25:23 +0100

    mount database to folder not as file

--- fishing_exam_alert/db.py
+++ fishing_exam_alert/db.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel, create_engine
 
-sqlite_file_name = "database.sqlite"
+sqlite_file_name = "db/database.sqlite"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url)

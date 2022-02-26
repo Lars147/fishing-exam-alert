8250d346c59ad613e5cef70a4deb45d4722ce8e8
larsheinen <https://meet.google.com/yqv-jgpo-cjr?hs=151>
2022-02-26 22:16:40 +0100

    replace prints with logger

--- fishing_exam_alert/confirmation.py
+++ fishing_exam_alert/confirmation.py
@@ -1,4 +1,5 @@
 import time
+from loguru import logger
 
 from fishing_exam_alert import models, notifier
 from fishing_exam_alert.settings import setting
@@ -12,9 +13,9 @@ def main():
 
     not_notified_row_index = gsheet.get_index_of_first_not_notified_record()
     if not_notified_row_index < 0:
-        print("No records found that need to be notified. Exiting script...")
+        logger.info("No records found that need to be notified. Exiting script...")
         return
-    print(f"Run notification script for index {not_notified_row_index}...")
+    logger.info(f"Run notification script for index {not_notified_row_index}...")
 
     row_to_notify = gsheet.df.iloc[not_notified_row_index]
 
@@ -22,13 +23,13 @@ def main():
 
     # send mail
     if row_to_notify["An- oder Abmeldung?"] == "Anmeldung / Aktualisierung":
-        print(f"Send confirmation mail to {notification_row['email_notify']}...")
+        logger.info(f"Send confirmation mail to {notification_row['email_notify']}...")
         notifier.send_confirmation_mail(notification_row["email_notify"], notification_row["filters"])
     elif row_to_notify["An- oder Abmeldung?"] == "Abmeldung":
-        print(f"Send unsubscribe mail to {notification_row['email_notify']}...")
+        logger.info(f"Send unsubscribe mail to {notification_row['email_notify']}...")
         notifier.send_unsubscribe_mail(notification_row["email_notify"])
     else:
-        print(f"Unknown value for An- oder Abmeldung?: {row_to_notify['An- oder Abmeldung?']}")
+        logger.info(f"Unknown value for An- oder Abmeldung?: {row_to_notify['An- oder Abmeldung?']}")
 
     gsheet.mark_row_as_notified(not_notified_row_index)
 
@@ -36,4 +37,5 @@ def main():
 if __name__ == "__main__":
     while True:
         main()
+        logger.info(f"Sleep for {setting.CONFIRMATION_INTERVAL_SECONDS} seconds...")
         time.sleep(setting.CONFIRMATION_INTERVAL_SECONDS)

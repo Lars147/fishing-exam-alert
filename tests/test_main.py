3f3bc5d78b16abdf427bb22830e30dedca3bb694
larsheinen <https://meet.google.com/yqv-jgpo-cjr?hs=151>
2022-03-06 21:25:40 +0100

    update test utils for exam

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,12 +9,12 @@ from fishing_exam_alert import db, main
 class TestMain(unittest.TestCase):
     def test_get_active_exams_with_duration(self):
         with Session(db.engine) as session:
-            test_user = create_random_user(session)
-            test_exam = create_random_exam(session)
+            test_user = create_random_user(session, need_headphones=False, need_disabled_access=False, active=True)
+            test_exam = create_random_exam(session, exam_id="0001")
 
             active_exams = main.get_active_exams(session, test_user)
 
-        self.assertTrue(len(active_exams) == 1)
+        self.assertTrue(len(active_exams) >= 1)
         self.assertTrue(active_exams.iloc[0]["exam_id"] == test_exam.exam_id)
         self.assertTrue(active_exams.iloc[0]["address_line"] > test_user.get_address_line())
         self.assertTrue(active_exams.iloc[0]["travel_distance"] > 0)
--- tests/utils.py
+++ tests/utils.py
@@ -70,27 +70,90 @@ def create_random_user(
     return user
 
 
-def create_random_exam(db: Session) -> models.Exam:
-    exam_defaults = get_random_valid_exam_addresses()
-    exam_defaults["name"] = "Test Exam"
-    exam_defaults["status"] = "Frei"
-    exam_defaults["exam_start"] = datetime.now() + timedelta(weeks=1)
-    exam_defaults["min_participants"] = 1
-    exam_defaults["max_participants"] = 10
-    exam_defaults["current_participants"] = 7
-    exam_defaults["disabled_access"] = True
-    exam_defaults["headphones"] = True
+def get_random_exam(
+    id: Optional[int] = None,
+    exam_id: Optional[str] = None,
+    name: Optional[str] = None,
+    street: Optional[str] = None,
+    street_number: Optional[str] = None,
+    city: Optional[str] = None,
+    postal_code: Optional[str] = None,
+    district: Optional[models.District] = None,
+    exam_start: Optional[datetime] = None,
+    min_participants: Optional[int] = None,
+    max_participants: Optional[int] = None,
+    current_participants: Optional[int] = None,
+    status: Optional[str] = None,
+    disabled_access: Optional[bool] = None,
+    headphones: Optional[bool] = None,
+    locale: str = "de_DE",
+) -> models.Exam:
+    fake = Faker(locale=locale)
 
-    exam, _ = models.Exam.get_or_create(db=db, exam_id="0001", defaults=exam_defaults)
+    min_participants = min_participants or random.randint(1, 40)
+    max_participants = max_participants or random.randint(min_participants, min_participants + 100)
+    current_participants = current_participants or random.randint(min_participants, max_participants)
+    if not status:
+        status = "Belegt" if current_participants == max_participants else "Frei"
+
+    return models.Exam(
+        id=id,
+        exam_id=exam_id or str(random.randint(1, 9999)),
+        name=name or fake.name(),
+        street=street or fake.street_name(),
+        street_number=street_number or fake.building_number(),
+        city=city or fake.city(),
+        postal_code=postal_code or fake.postalcode(),
+        district=district or random.choice(list(models.District)),
+        exam_start=exam_start or datetime.now() + timedelta(weeks=1),
+        min_participants=min_participants,
+        max_participants=max_participants,
+        current_participants=current_participants,
+        status=status,
+        disabled_access=disabled_access or fake.boolean(),
+        headphones=headphones or fake.boolean(),
+        created_at=None,
+        updated_at=None,
+    )
+
+
+def create_random_exam(
+    db: Session,
+    exam_id: Optional[str] = None,
+    name: str = "Test Exam",
+    street: str = "Herzogspitalstraße",
+    street_number: str = "24",
+    city: str = "München",
+    postal_code: str = "80331",
+    district: models.District = models.District.Oberbayern,
+    exam_start: Optional[datetime] = None,
+    min_participants: int = 1,
+    max_participants: int = 10,
+    current_participants: int = 7,
+    status: str = "Frei",
+    disabled_access: Optional[bool] = None,
+    headphones: Optional[bool] = None,
+) -> models.Exam:
+
+    exam_in = get_random_exam(
+        exam_id=exam_id,
+        name=name,
+        street=street,
+        street_number=street_number,
+        city=city,
+        postal_code=postal_code,
+        district=district,
+        exam_start=exam_start,
+        min_participants=min_participants,
+        max_participants=max_participants,
+        current_participants=current_participants,
+        status=status,
+        disabled_access=disabled_access,
+        headphones=headphones,
+    )
+
+    defaults = exam_in.dict()
+
+    exam, _ = models.Exam.get_or_create(db=db, exam_id=exam_in.exam_id, defaults=defaults)
 
     return exam
-
-
-def get_random_valid_exam_addresses() -> Dict[str, Any]:
-    return {
-        "street": "Herzogspitalstraße",
-        "street_number": "24",
-        "city": "München",
-        "postal_code": "80331",
-        "district": models.District.Oberbayern,
-    }
